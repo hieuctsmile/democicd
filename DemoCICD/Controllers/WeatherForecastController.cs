62fd775ce8d0862a8a4710a8d4cd2171a1145f46
Bazemin-H <>
2024-02-20 15:33:19 +0700

    upd

--- DemoCICD/Controllers/WeatherForecastController.cs
+++ DemoCICD/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoCICD.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Bazemin"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Bazemin","Romas"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
